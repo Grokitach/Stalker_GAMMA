5788a9e9a780c5d0d744431065b749c910c7140e
Grokitach <Grokitach@users.noreply.github.com>
2022-09-06 19:54:15 +0200

    GGX Specular by LVutner

---
+++ G.A.M.M.A/modpack_addons/188- Enhanced Shaders - KennShade/gamedata/shaders/r3/pbr_settings.h
@@ -0,0 +1,15 @@
+//=================================================================================================
+//Settings for PBR conversion
+//=================================================================================================
+//#define USE_PBR //Use physically based specular 
+#define USE_GGX_SPECULAR //use more expensive GGX specular
+//=================================================================================================
+#define ALBEDO_AMOUNT 1.00
+
+#define ROUGHNESS_LOW 0.4
+#define ROUGHNESS_HIGH 1.00
+#define ROUGHNESS_POW 0.25
+
+#define SPECULAR_BASE 0.056
+#define SPECULAR_RANGE 1
+#define SPECULAR_POW 1
