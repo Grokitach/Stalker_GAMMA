53b92cae7daf5971c878e0ac91b88ca0a3322281
Grokitach <Grokitach@users.noreply.github.com>
2022-09-21 08:26:09 +0200

    Shaders params: ok guns, no shiny ground, ok rain reflections

--- G.A.M.M.A/modpack_addons/Shaders cumulative pack for GAMMA/gamedata/shaders/r3/pbr_settings.h
+++ G.A.M.M.A/modpack_addons/Shaders cumulative pack for GAMMA/gamedata/shaders/r3/pbr_settings.h
@@ -1,15 +1,15 @@
 //=================================================================================================
 //Settings for PBR conversion
 //=================================================================================================
-#define USE_GGX_SPECULAR //use more expensive GGX specular
 #define USE_PBR //Use physically based specular 
+#define USE_GGX_SPECULAR //use more expensive GGX specular
 //=================================================================================================
-#define ALBEDO_AMOUNT 0.40
+#define ALBEDO_AMOUNT 1.00
 
-#define ROUGHNESS_LOW 0.80
-#define ROUGHNESS_HIGH 1.00
-#define ROUGHNESS_POW 0.35
+#define ROUGHNESS_LOW 0.01
+#define ROUGHNESS_HIGH 2.00
+#define ROUGHNESS_POW 0.01
 
 #define SPECULAR_BASE 0.080
 #define SPECULAR_RANGE 1
-#define SPECULAR_POW 1
+#define SPECULAR_POW 1.5
