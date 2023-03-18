31252729abca8df8afcb2dcbcaae2e99e6762d44
Grokitach <Grokitach@users.noreply.github.com>
2023-03-18 14:00:01 +0100

    Reduced some shaders quality for performance + sky NVG fix

---
+++ G.A.M.M.A/modpack_addons/296- Atmospherics Shaders Weathers and Reshade - Hippobot/gamedata/shaders/r3/settings_screenspace_FOG.h
@@ -0,0 +1,7 @@
+// [ SETTINGS ] [ FOG ]
+
+#define G_FOG_HEIGHT					9.35f	// Height of the ground fog.
+#define G_FOG_HEIGHT_INTENSITY			1.1f	// Intensity of the ground fog.
+#define G_FOG_HEIGHT_DENSITY			1.4f	// Density of the ground fog.
+
+#define G_FOG_SUNCOLOR_INTENSITY		0.11f	// Intensity of sun color in the fog. [ 0.0f = 0% | 1.0f = 100% ]
