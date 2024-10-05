46be95f9656970f200262599c133243ce8265f82
Grokitach <Grokitach@users.noreply.github.com>
2024-10-05 18:23:03 +0200

    Engine update v2024.10.5

--- G.A.M.M.A/modpack_patches/gamedata/shaders/r3/effects_sun.s
+++ G.A.M.M.A/modpack_patches/gamedata/shaders/r3/effects_sun.s
@@ -1,5 +1,5 @@
 function normal(shader, t_base, t_second, t_detail)
-  shader:begin("effects_sun","stub_default")
+  shader:begin("effects_sun","effects_sun")
 --        : sorting        (2, false)
 --        : blend                (true,blend.srcalpha,blend.invsrcalpha)
 --        : blend                (true,blend.one,blend.one)
