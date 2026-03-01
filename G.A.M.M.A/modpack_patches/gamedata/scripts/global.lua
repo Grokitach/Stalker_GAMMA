1e3a0434e1eb1589064cb64b6896fca9ce1be63f
Grokitach <Grokitach@users.noreply.github.com>
2026-03-01 12:03:58 +0100

    Anomaly modded exe v2026.3.1 MT by Demonized

--- G.A.M.M.A/modpack_patches/gamedata/scripts/global.lua
+++ G.A.M.M.A/modpack_patches/gamedata/scripts/global.lua
@@ -8,34 +8,60 @@ math.mod = math.fmod
 DebuggerMode = false
 
 function debug_jit_off()
-	if DebuggerMode then
-		if jit then jit.off() end
-	end
+	if jit then jit.off() end
 end
 
 function debug_jit_on()
+	if jit then jit.on() end
+end
+
+function debugger_attach()
 	if DebuggerMode then
-		if jit then jit.on() end
+		debugger_reconnect()
+	else
+		debugger_start()
 	end
 end
 
-function debugger_attach() 
+function debugger_start()
 	if DebuggerMode then
-		log('LuaPanda: Disabling jit...')
-		debug_jit_off()
-		log('LuaPanda: Reconnecting')
-		LuaPanda.reConnect()
-		log('LuaPanda: reconnected')
-		debug_jit_on()
-		log('LuaPanda: Started')
-	else
-		log('LuaPanda: Disabling jit...')
-		debug_jit_off()
-		log('LuaPanda: Connecting')
-		LuaPanda.start("127.0.0.1", 8818)
-		log('LuaPanda: Connected')
-		DebuggerMode = true
-		debug_jit_on()
-		log('LuaPanda: Started')
+		return
 	end
+
+	log('LuaPanda: Disabling jit...')
+	debug_jit_off()
+	log('LuaPanda: Connecting')
+	LuaPanda.start("127.0.0.1", 8818)
+	log('LuaPanda: Connected')
+	DebuggerMode = true
+	debug_jit_on()
+	log('LuaPanda: Started')
+end
+
+function debugger_reconnect()
+	if not DebuggerMode then
+		return
+	end
+
+	log('LuaPanda: Disabling jit...')
+	debug_jit_off()
+	log('LuaPanda: Reconnecting')
+	LuaPanda.reConnect()
+	log('LuaPanda: reconnected')
+	debug_jit_on()
+	log('LuaPanda: Started')
+end
+
+function debugger_stop()
+	if not DebuggerMode then
+		return
+	end
+
+	log('LuaPanda: Disabling jit...')
+	debug_jit_off()
+	log('LuaPanda: Disconnecting')
+	LuaPanda.stopAttach()
+	log('LuaPanda: Disconnected')
+	DebuggerMode = false
+	log('LuaPanda: Stopped')
 end
